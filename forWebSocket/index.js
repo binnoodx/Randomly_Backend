ca5e68e96cc730a51459a7508cc776fddf49e2c6
Binod Sharma <thebinod404@gmail.com>
2025-07-22 23:23:38 +0545

    day #3 - Show realtime Online Users

--- forWebSocket/index.js
+++ forWebSocket/index.js
@@ -16,33 +16,32 @@ const io = new Server(server, {
 });
 
 // { socketId: string, email: string }
+
 let users = [];
 
 io.on("connection", (socket) => {
 
   socket.on("newUser", (email) => {
     
-    // Check if email is already added (avoid duplicates)
     const exists = users.some((user) => user.email === email);
     
     if (!exists && email) {
-      console.log("📥 New user:", email);
-      users.push({ socketId: socket.id, email });
+      console.log("📥 New user:", email)
+      users.push({ socketId: socket.id, email })
     }
 
+    // Send Only Email to Frontend
     const emailList = users.map(u => u.email);
     io.emit("sendUserArray", emailList);
   });
-
+  
   socket.on("disconnect", () => {
-    console.log("❌ User disconnected:", socket.id);
-
-    // Remove user by socket ID
+    
     users = users.filter((user) => user.socketId !== socket.id);
-
     const emailList = users.map(u => u.email);
-    io.emit("sendUserArray", emailList);
+    io.emit("sendUserArray", emailList)
   });
 });
 
 server.listen(8000, () => console.log("🚀 Server running on port 8000"));
+
